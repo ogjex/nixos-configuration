c431171b65dc628e28f60d9cd59503d3ddb1fc64
ogjex <m.agnu.ha@gmail.com>
2026-04-07 09:58:42 +0200

    update with flake experimental features

--- configuration.nix
+++ configuration.nix
@@ -40,6 +40,8 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # enable flakes
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   networking.hostName = "nixos-work"; # Define your hostname.
   networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
