a9e624ee8ded3a020006d4f0795d39b032ac01d2
ogjex <m.agnu.ha@gmail.com>
2026-04-17 10:21:55 +0200

    add dislocker and remove nixvim (must be a flake)

--- configuration.nix
+++ configuration.nix
@@ -110,7 +110,6 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    nixvim
     neovim
     git
     wget
