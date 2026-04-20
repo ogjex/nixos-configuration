1d455e107528ea28b902dfe7ecb0aeeeb6252f7e
ogjex <m.agnu.ha@gmail.com>
2026-04-20 16:16:29 +0200

    add structure for nixvim

---
+++ nixvim/options.nix
@@ -0,0 +1,71 @@
+{pkgs, ...}: {
+  config = {
+    viAlias = true;
+    vimAlias = true;
+
+    opts = {
+      updatetime = 100;
+      number = true;
+      relativenumber = true;
+
+      autoindent = true;
+      autowrite = true;
+      confirm = true;
+      clipboard = "unnamedplus";
+      cursorline = true;
+      list = true;
+      expandtab = true;
+      shiftround = true;
+      shiftwidth = 2;
+      signcolumn = "yes";
+      smartcase = true;
+      smartindent = true;
+      tabstop = 2;
+
+      ignorecase = true;
+      incsearch = true;
+      completeopt = "menu,menuone,noselect";
+      wildmode = "longest:full,full";
+
+      swapfile = false;
+      undofile = true;
+      undolevels = 10000;
+
+      spell = false;
+      spelllang = "en_us";
+    };
+    autoCmd = [
+      {
+        event = "FileType";
+        pattern = ["markdown" "text" "gitcommit" "plaintex" "tex"];
+        command = "setlocal spell";
+        desc = "Enable spell checking for prose filetypes";
+      }
+    ];
+    # Only enable wl-copy on Linux systems
+    clipboard.providers.wl-copy.enable = pkgs.stdenv.isLinux;
+    extraConfigLua = ''
+      vim.opt.mouse=""
+
+      -- Map common extension variants to core filetypes.
+      vim.filetype.add({
+        extension = {
+          mdx = "markdown",
+          tfvars = "terraform",
+        },
+        filename = {
+          [".gitlab-ci.yml"] = "yaml",
+          [".gitlab-ci.yaml"] = "yaml",
+          ["docker-compose.yml"] = "yaml",
+          ["docker-compose.yaml"] = "yaml",
+          ["compose.yml"] = "yaml",
+          ["compose.yaml"] = "yaml",
+        },
+        pattern = {
+          [".*values[^/]*%.ya?ml"] = "yaml",
+        },
+      })
+
+    '';
+  };
+}
