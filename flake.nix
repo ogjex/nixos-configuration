7a05bb00bbf1b67b07ca3bfec7f98b233ac63567
ogjex <m.agnu.ha@gmail.com>
2026-04-20 15:33:39 +0200

    add nixvim to flake

--- flake.nix
+++ flake.nix
@@ -3,8 +3,13 @@
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 		mangowm = {
 			url = "github:mangowm/mango";
-			inputs.nixpkgs.follows = "nixpkgs";
 		};
+		inputs.nixvim = {
+		    url = "github:nix-community/nixvim";
+		    # If you are not running an unstable channel of nixpkgs, select the corresponding branch of Nixvim.
+		    # url = "github:nix-community/nixvim/nixos-25.11";
+		};
+		inputs.nixpkgs.follows = "nixpkgs";
 		
 	};
 
