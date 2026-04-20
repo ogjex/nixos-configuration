7600ebaef7e47acfec2fcb5c5846b25e5e61c0b5
ogjex <m.agnu.ha@gmail.com>
2026-04-20 15:52:13 +0200

    remove neovim

--- configuration.nix
+++ configuration.nix
@@ -116,7 +116,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    neovim
+    #neovim
     git
     wget
     curl
