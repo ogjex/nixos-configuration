ba3852353090034efe296ac19743e00512029e45
ogjex <m.agnu.ha@gmail.com>
2026-04-20 15:37:00 +0200

    correct nixvim as input

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 			url = "github:mangowm/mango";
 			inputs.nixpkgs.follows = "nixpkgs";
 		};
-		inputs.nixvim = {
+		nixvim = { 
 		    url = "github:nix-community/nixvim";
 		    # If you are not running an unstable channel of nixpkgs, select the corresponding branch of Nixvim.
 		    # url = "github:nix-community/nixvim/nixos-25.11";
