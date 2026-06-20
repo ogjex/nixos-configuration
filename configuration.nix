d4515e51ce61fc0bf2fe61c35c5bb0c709a6a6b7
ogjex <m.agnu.ha@gmail.com>
2026-06-20 16:43:59 +0200

    update nixvim into configuration

--- configuration.nix
+++ configuration.nix
@@ -172,9 +172,9 @@
     signal-desktop
     # terminal fluff and stuff
     zsh
-    bat
     fzf
     highlight
+    cat
     bat
     tmux    
     vifm
@@ -218,9 +218,46 @@
   };
   programs.nixvim = {
     enable = true;
-
+    defaultEditor = true;
     colorschemes.catppuccin.enable = true;
     plugins.lualine.enable = true;
+	  config = {
+	    viAlias = true;
+	    vimAlias = true;
+
+		    opts = {
+		      updatetime = 100;
+		      number = true;
+		      relativenumber = true;
+
+		      autoindent = true;
+		      autowrite = true;
+		      confirm = true;
+		      clipboard = "unnamedplus";
+		      cursorline = true;
+		      list = true;
+		      expandtab = true;
+		      shiftround = true;
+		      shiftwidth = 2;
+		      signcolumn = "yes";
+		      smartcase = true;
+		      smartindent = true;
+		      tabstop = 2;
+
+		      ignorecase = true;
+		      incsearch = true;
+		      completeopt = "menu,menuone,noselect";
+		      wildmode = "longest:full,full";
+
+		      swapfile = false;
+		      undofile = true;
+		      undolevels = 10000;
+
+		      spell = false;
+		      spelllang = "en_us";
+		    };
+		};
+
   };
   # .............................................................................................................................................................................................
   # git
@@ -367,4 +404,3 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
 
-}
