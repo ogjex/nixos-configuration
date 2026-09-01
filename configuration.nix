88faf3d00208e3de22f02bffe996117ded3db948
ogjex <m.agnu.ha@gmail.com>
2026-09-01 09:23:58 +0200

    add git-crypt package

--- configuration.nix
+++ configuration.nix
@@ -124,6 +124,7 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     git
+    git-crypt
     wget
     curl
     nmap
