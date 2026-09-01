be9ca73232fffe47c753e4122b9c136e98154e8b
ogjex <m.agnu.ha@gmail.com>
2026-09-01 10:23:43 +0200

    change to pintentryPackage set to gtk2

--- configuration.nix
+++ configuration.nix
@@ -476,7 +476,7 @@
   services.pcscd.enable = true;
   programs.gnupg.agent = {
     enable = true;
-    pinentryPackage = gtk2;
+    pinentryPackage = "gtk2";
     enableSSHSupport = true;
   };
   services.fwupd.enable = true;
